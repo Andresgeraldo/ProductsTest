b69320fcfb5df56bc1b0342df54e9ef6810a57d1
Starling Germosen <sgermosen@praysoft.net>
2018-02-28 14:13:39 -0400

    Cambios al api estableciendo las respuestas mas detalladas

--- ProductsTest.API/Controllers/CategoriesController.cs
+++ ProductsTest.API/Controllers/CategoriesController.cs
@@ -1,22 +1,53 @@
-﻿using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Description;
-using ProductsTest.Domain;
-
-namespace ProductsTest.API.Controllers
+﻿namespace ProductsTest.API.Controllers
 {
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.Http.Description;
+    using ProductsTest.API.Models;
+    using ProductsTest.Domain;
+
     public class CategoriesController : ApiController
     {
         private DataContext db = new DataContext();
 
         // GET: api/Categories
-        public IQueryable<Category> GetCategories()
+        public async Task<IHttpActionResult> GetCategories()
         {
-            return db.Categories;
+            var categories = await db.Categories.ToListAsync();
+            var categoriesResponse = new List<CategoryResponse>();
+
+            foreach (var category in categories)
+            {
+                var productsResponse = new List<ProductResponse>();
+
+                foreach (var product in category.Products)
+                {
+                    productsResponse.Add(new ProductResponse {
+                    Description= product.Description,
+                    Image=product.Image,
+                    IsActive= product.IsActive,
+                    LastPurchase= product.LastPurchase,
+                    Price=product.Price,
+                    ProductId=product.ProductId,
+                    Remarks=product.Remarks,Stock=product.Stock});
+
+                }
+
+                categoriesResponse.Add(new CategoryResponse
+                {
+                    CategoryId=category.CategoryId,
+                    Description=category.Description,
+                    Products=productsResponse
+                });
+
+            }
+
+            return Ok(categoriesResponse);
         }
 
         // GET: api/Categories/5
--- ProductsTest.API/Models/CategoryResponse.cs
+++ ProductsTest.API/Models/CategoryResponse.cs
@@ -1,26 +1,20 @@
-﻿namespace ProductsTest.Domain
+﻿namespace ProductsTest.API.Models
 {
-    using Newtonsoft.Json;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
-    public class Category
+    public class CategoryResponse
     {
-        [Key]
+        
         public int CategoryId { get; set; }
-
-        [Required(ErrorMessage = "El Campo {0} es Requerido")]
-        [MaxLength(50, ErrorMessage = "El Campo {0} solo puede tener {1} caracteres de longitud")]
-        [Index("Category_Description_Index", IsUnique = true)]
+      
         public string Description { get; set; }
 
         //el objeto categoria que llamamos del api no devuelve el objeto productos por esta anotacion, 
         //sin embargo debemos dejarla por los problemas que trae esta a nivel de base de datos, ademas de que el api no sabe deserializar un objeto virtual
         //para estos fines entonces debemos crear una clase response que tiene los mismos atributos que esta clase, pero sin las anotaciones
         //en donde la propiedad virtual la debemos cambiar por una lista
-        [JsonIgnore]
-        public virtual ICollection<Product> Products { get; set; }
+        
+        public List<ProductResponse> Products { get; set; }
 
     }
 }
